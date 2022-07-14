30497cf9cd315857be93cca5253e730f33b684c0
Valentyn <valentyn.lugovyi@gmail.com>
2022-07-14 14:09:32 +0300

    Updates

---
+++ precache-manifest.b5726c67128dfbd66d044599850b7139.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d04b1f2ab3739fb03dd61d7a45f117",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "31ca06394f81beb1e66c",
+    "url": "/todolist/static/css/main.a365ad36.chunk.css"
+  },
+  {
+    "revision": "32e39f8f3f635a395fdc",
+    "url": "/todolist/static/js/2.529ae15e.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.529ae15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ca06394f81beb1e66c",
+    "url": "/todolist/static/js/main.fda500f5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/todolist/static/js/main.fda500f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b52fcafdcdaa9c087e",
+    "url": "/todolist/static/js/runtime-main.28eb14c4.js"
+  }
+]);
